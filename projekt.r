c4cdc277215ebdae33f84e4fa1ca2c942970c199
Jakob Lenarcic <jakob.lenarcic@amis.net>
2015-01-10 22:43:26 +0100

    popravek, konec tretje faze

--- projekt.r
+++ projekt.r
@@ -1,13 +1,13 @@
 # Pobrišemo PDF-je in počistimo delovno okolje
 silent <- TRUE
-source("clearpdf.r")
+source("clearpdf.r", encoding = "UTF-8")
 
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r")
-source("slike/Grafi.R")
+source("uvoz/uvoz.r", encoding = "UTF-8")
+source("slike/Grafi.R", encoding = "UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
-source("vizualizacija/vizualizacija.r")
+source("vizualizacija/vizualizacija.r", encoding = "UTF-8")
 
 # # 4. faza: Napredna analiza podatkov
 # source("analiza/analiza.r")
